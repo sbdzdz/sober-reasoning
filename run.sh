f00b60fb21fa882e1c57a3781c69185c928f5f55
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2025-12-07 21:10:19 +0100

    Modify the main script to use mergerl models

--- run.sh
+++ run.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-LOCAL_DIR="<PATH TO LOCAL REPO>"
-OUTPUT_DIR="<PATH TO OUTPUT DIR>"
-PARTITION=<PARTITION>
-VENV=<PATH TO VENV>/bin/activate
+LOCAL_DIR="/weka/bethge/dziadzio08/sober-reasoning"
+OUTPUT_DIR="/weka/bethge/dziadzio08/sober-reasoning/mergerl"
+PARTITION="h100-ferranti"
+VENV="/weka/bethge/dziadzio08/sober-reasoning/.venv/bin/activate"
 mkdir -p $OUTPUT_DIR/logs
 
 MODELS=(
-    deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
-    deepseek-ai/DeepSeek-R1-Distill-Qwen-7B
-    knoveleng/Open-RS3
-    knoveleng/Open-RS2
-    knoveleng/Open-RS1
-    agentica-org/DeepScaleR-1.5B-Preview
-    simplescaling/s1.1-7B
-    open-thoughts/OpenThinker-7B
-    Intelligent-Internet/II-Thought-1.5B-Preview
-    # sail/Qwen2.5-Math-1.5B-Oat-Zero
-    # simplescaling/s1.1-32B
-    # deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
-    # open-thoughts/OpenThinker-32B
-    # GAIR/LIMO
-    # bespokelabs/Bespoke-Stratos-32B
+    iaa01/llama-8b-merge-alpha05-freq10
+    iaa01/llama-8b-merge-alpha08-freq10
+    iaa01/llama-8b-merge-alpha1-freq10
+    iaa01/llama-8b-grpo-kl
+    iaa01/llama-8b-grpo-no-kl
 )
 
 TOP_PS=(
@@ -38,10 +28,7 @@ MAX_MODEL_LENGTHS=(
 )
 
 MAX_TOKENS_LIST=(
-    # 4096
-    # 8192
-    # 16384
-    32768
+    16384
 )
 
 for MAX_MODEL_LENGTH in "${MAX_MODEL_LENGTHS[@]}"; do
@@ -78,10 +65,9 @@ TASKS=(
     "custom|aime24|0|0"
     "custom|math_500|0|0"
     "custom|amc23|0|0"
-    # "custom|aime25|0|0"
-    # "custom|gpqa:diamond|0|0"
-    # "custom|minerva|0|0"
-    # "custom|olympiadbench|0|0"
+    "custom|aime25|0|0"
+    "custom|minerva|0|0"
+    "custom|olympiadbench|0|0"
 )
 for SEED in "\${SEEDS[@]}"; do
 for TASK in "\${TASKS[@]}"; do
@@ -94,8 +80,7 @@ for TASK in "\${TASKS[@]}"; do
         --output_dir $OUTPUT_DIR \
         --max_new_tokens $MAX_TOKENS \
         --max_model_length $MAX_MODEL_LENGTH \
-        --custom_tasks_directory lighteval_tasks.py \
-        --use_chat_template
+        --custom_tasks_directory lighteval_tasks.py
 done
 done
 EOT
