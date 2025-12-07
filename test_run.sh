349a465e0048749a90ffd8d8be13dbeef9af3425
Sebastian <sebastian.dziadzio@gmail.com>
2025-12-07 21:25:38 +0100

    Add a test script

---
+++ test_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MODEL="iaa01/llama-8b-merge-alpha05-freq10"
+TASK="custom|math_500|0|0"
+SEED=0
+TEMP=0.8
+TOP_P=0.9
+MAX_TOKENS=16384
+MAX_MODEL_LENGTH=32768
+OUTPUT_DIR="./test_output"
+
+python main.py \
+    --model $MODEL \
+    --task $TASK \
+    --temperature $TEMP \
+    --top_p $TOP_P \
+    --seed $SEED \
+    --output_dir $OUTPUT_DIR \
+    --max_new_tokens $MAX_TOKENS \
+    --max_model_length $MAX_MODEL_LENGTH \
+    --custom_tasks_directory lighteval_tasks.py
+
